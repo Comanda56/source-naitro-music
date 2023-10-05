d164f8709c5f7eeaf88f07a2e44e5cec0f99016b
Comanda56 <123072919+Comanda56@users.noreply.github.com>
2023-10-05 16:58:38 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["DARK_x7"]
+OWNER_NAME = "nAiTrO"
+BOT_TOKEN = "5859617352:AAHobyaU1qduIIRNjq2b44-LR-pu7_GxtD8"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/NA_44_ROo"
+GROUP = "https://t.me/NA_44_ROo"
+VIDEO = "https://telegra.ph/file/f2240d4ae2732459c8a51.jpg"
+LOGS = "nAiTrO2"
+
