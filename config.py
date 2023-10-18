7b190fd8c4737afb9312e1a75932f7fe6dfd1a85
Comanda56 <123072919+Comanda56@users.noreply.github.com>
2023-10-18 22:33:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+from OWNER import BOT_TOKEN, OWNER, OWNER_NAME, DATABASE, CHANNEL, GROUP, LOGS, VIDEO
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv(.)
+admins = {6346836245}
+user = {@DARK_x7}
+call = {}
+dev = {@DARK_x7}
+logger = {.}
+logger_mode = {.}
+botname = {𝘽𝙤𝙏#𝙉𝙣.𝘼𝙖.𝙞.𝙏𝙩.𝙍𝙧.𝙤💎🕷️}
+appp = {.}
+helper = {@KK1c1}
+
+
+
+API_ID = int(getenv("API_ID", "15387674"))
+API_HASH = getenv("API_HASH", "a98fa4e58aaab13edb1f9275fd76efbe")
+BOT_TOKEN = 5859617352:AAHobyaU1qduIIRNjq2b44-LR-pu7_GxtD8
+MONGO_DB_URL = 64651bab919a525757141da1
+OWNER = @DARK_x7
+OWNER_NAME = nAiTrO"( ꪀاوفــــ͢ـݪايـ ـ )⋆🇰🇼
+CHANNEL = @AL_ml_ok
+GROUP = https://t.me/+VWQvcVNBdrc0NzI0
+LOGS = .
+VIDEO = .
